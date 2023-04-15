924a5a1b36924ac874ea2ee449cb9cc82f084886
santidotpy <santidotpy@gmail.com>
2023-04-15 17:58:44 -0300

    add messageError

--- entrega_session/src/utils/messageError.js
+++ entrega_session/src/utils/messageError.js
@@ -2,31 +2,37 @@ import passport from "passport";
 
 //Funcion general para retornar errores en las estrategias de Passport
 export const passportError = (strategy) => {
-    return async (req, res, next) => {
-        passport.authenticate(strategy, (error, user, info) => {
-            if (error) {
-                return next(error)
-            }
-            if (!user) { //Si no existe mi usuario
-                return res.status(401).send({ error: info.messages ? info.messages : info.toString() }) //Si existe una propiedad messages en info, la envio sino envio pasado a String el objeto info
-            }
+  return async (req, res, next) => {
+    passport.authenticate(strategy, (error, user, info) => {
+      if (error) {
+        return next(error);
+      }
+      if (!user) {
+        //Si no existe mi usuario
+        //return done(null, false, { message: "User not found" }); //Retorno el error
+        return res
+          .status(401)
+          .send({ error: info.messages ? info.messages : info.toString() }); //Si existe una propiedad messages en info, la envio sino envio pasado a String el objeto info
+      }
 
-            req.user = user
-            next()
-        })(req, res, next)
+      req.user = user;
+      next();
+    })(req, res, next);
+  };
+};
 
+export const authorization = () => {
+  return async (req, res, next) => {
+    if (!req.user) {
+      //No hay un usuario
+      return res.status(401).send({ error: "Unauthorized user" });
     }
-}
-
-export const authorization = (rol) => {
-    return async (req, res, next) => {
-        if (!req.user) {  //No hay un usuario
-            return res.status(401).send({ error: "User no autorizado" })
-        }
-        console.log(req.user.user[0]) //Acceso a las propiedades del user en JWT
-        if (req.user.user[0].rol != rol) {
-            return res.status(403).send({ error: "User no tiene los permisos necesarios" })
-        }
-        next()
+    // console.log(req.user.user.isadmin); //Acceso a las propiedades del user en JWT
+    if (!req.user.user.isadmin) {
+      return res
+        .status(403)
+        .send({ error: "You do not have permission to access" });
     }
-}
+    next();
+  };
+};
