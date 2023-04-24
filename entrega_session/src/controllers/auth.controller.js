68753b85ae1bf647abfbbd433aba9ce23416d6b3
santidotpy <santidotpy@gmail.com>
2023-04-24 15:38:43 -0300

    ✨ add auth.controller

--- entrega_session/src/controllers/auth.controller.js
+++ entrega_session/src/controllers/auth.controller.js
@@ -1,31 +1,62 @@
-const getAuth = (req, res, next) => {
-  if (req.session.login) {
-    // if user is logged in
-    res.redirect("api/products");
-  } else {
-    // if user is not logged in
-    res.redirect("auth/login");
+import passport from "passport";
+import jwt from "jsonwebtoken";
+import { validatePassword } from "../utils/bcrypt.js";
+import { UserMongo } from "../dao/MongoDB/models/User.js";
+
+export const managerUser = new UserMongo();
+
+export const getUsers = async (req, res, page) => {
+  try {
+    const page = req.query.page || 1;
+    const users = await managerUser.getElements(page);
+    res.send(users);
+  } catch (e) {
+    res.status(500).send({ status: "error", message: "Internal Server Error" });
   }
 };
 
-const validateLogin = (req, res, next) => {
-  const { email, password } = req.body;
-  if (email === "mail@mail.com" && password === "password123") {
-    // if user is logged in
+export const loginValidation = async (req, res, next) => {
+  try {
+    passport.authenticate('jwt', { session: false }, async (err, user, info) => {
+      if (err) {
+          return res.status(401).send("Error authenticating user")
+      }
+      if (user) {
+          //El token no existe, entonces consulto por el usuario
+          const { email, password } = req.body
+          const userBDD = await managerUser.getUserByEmail(email)
 
-    req.session.login = true;
-    res.redirect("api/products");
-  } else {
-    // if user is not logged in
-    res.render("auth/login", { error: "Invalid credentials" });
-  }
-  next();
-};
+          if (!userBDD) {
+              // UserBDD no encontrado en mi aplicacion
+              return res.status(401).send("User not found")
+          }
 
-const destroySession = (req, res, next) => {
-  if (req.session.login) {
-    req.session.destroy(() => {
-      res.redirect("api/login");
-    });
-  }
+          if (!validatePassword(password, userBDD.password)) {
+              // Contraseña no es válida
+              return res.status(401).send("Please check your credentials")
+          }
+
+          // Ya que el usuario es valido, genero un nuevo token
+          const token = jwt.sign({ user: { id: userBDD._id } }, process.env.PRIVATE_KEY_JWT)
+          res.cookie('jwt', token, { httpOnly: true })
+          return res.status(200).json({ token })
+      } else {
+          //El token existe, asi que lo valido
+          const token = req.cookies.jwt;
+          jwt.verify(token, process.env.PRIVATE_KEY_JWT, async (err, decodedToken) => {
+            if (err) {
+              // Token no valido
+              return res.status(401).send("Please check your credentials")
+            } else {
+              // Token valido
+                  req.user = user
+                  next()
+              }
+          })
+      }
+
+  })(req, res, next)
+} catch (error) {
+  res.status(500).send(`Ocurrio un error en Session, ${error}`)
+}
 };
