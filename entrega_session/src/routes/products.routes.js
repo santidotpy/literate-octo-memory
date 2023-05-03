d396048b0855811fe0b79515be8fd208b6bffa19
santidotpy <santidotpy@gmail.com>
2023-05-03 02:46:15 -0300

    🔒️ protect product routes

--- entrega_session/src/routes/products.routes.js
+++ entrega_session/src/routes/products.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { passportError, authorization } from "../utils/messageError.js";
 import { ProductMongo } from "../dao/MongoDB/models/Product.js";
 
 const managerProduct = new ProductMongo();
@@ -26,39 +27,85 @@ routerProd.get("/products", async (req, res) => {
 });
 
 // post para agregar productos
-routerProd.post("/products", async (req, res) => {
-  const products = req.body; // array of products
-  console.log(products);
-  const newProducts = await Promise.all(
-    products.map(async (product) => {
-      const {
+routerProd.post(
+  "/products",
+  passportError("jwt"),
+  authorization(),
+  async (req, res) => {
+    const products = req.body; // array of products
+    console.log(products);
+    const newProducts = await Promise.all(
+      products.map(async (product) => {
+        const {
+          productName,
+          description,
+          code,
+          price,
+          thumbnail,
+          stock,
+          status,
+          category,
+        } = product;
+        const newProduct = await managerProduct.addElements({
+          productName,
+          description,
+          code,
+          price,
+          thumbnail,
+          stock,
+          status,
+          category,
+        });
+        return newProduct;
+      })
+    );
+    console.log(newProducts);
+    res.render("products/all-products", {
+      title: "Products",
+      products: JSON.stringify(newProducts),
+    });
+  }
+);
+
+// eliminar un producto
+routerProd.delete(
+  "/products",
+  passportError("jwt"),
+  authorization(),
+  async (req, res) => {
+    const { id } = req.query;
+    try {
+      await managerProduct.deleteElement(id);
+      res.status(200).send({ status: "success", message: `${id} deleted` });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+// actualizar un producto
+routerProd.put(
+  "/products",
+  passportError("jwt"),
+  authorization(),
+  async (req, res) => {
+    const { id } = req.query;
+    const { productName, description, code, price, thumbnail, stock } =
+      req.body;
+    try {
+      const product = await managerProduct.updateElement(id, {
         productName,
         description,
         code,
         price,
         thumbnail,
         stock,
-        status,
-        category,
-      } = product;
-      const newProduct = await managerProduct.addElements({
-        productName,
-        description,
-        code,
-        price,
-        thumbnail,
-        stock,
-        status,
-        category,
       });
-      return newProduct;
-    })
-  );
-  console.log(newProducts);
-  res.render("products/all-products", {
-    title: "Products",
-    products: JSON.stringify(newProducts),
-  });
-});
+      res.status(200).send({ status: "success", message: "Product updated correctly" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export default routerProd;
